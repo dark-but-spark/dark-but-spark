d6394dfd8d7ef8fb116def64a30a4bcdad1d3210
chen jiyan <3036789610@qq.com>
2025-03-07 12:01:52 +0800

    Fix Pi calculation in RISC-V assembly by correcting loop condition and improving floating-point operations

--- riscv/Pi.asm
+++ riscv/Pi.asm
@@ -7,22 +7,28 @@
     li a3,1 #i
     li t2,0
     fcvt.s.w fa1,t2 #sum
+    li t2,1
+    fcvt.s.w fa6,t2
     loop:
-        fcvt.w.s fa2,a3 #i
+        fcvt.s.w fa2,a3 #i
         li t2,2
         fcvt.s.w fa3,t2
-        fmul.s fa4,fa3,f2 #i*2
+        fmul.s fa4,fa3,fa2 #i*2
         li t2,1
         fcvt.s.w fa3,t2
         fsub.s fa2,fa4,fa3 #i*2-1
         li t2,4
         fcvt.s.w fa3,t2
         fdiv.s fa4,fa3,fa2 #4/(i*2-1)
+        fmul.s fa4,fa4,fa6
+        li t2,0
+        fcvt.s.w fa3,t2
+        fsub.s fa6,fa3,fa6#反转符号
         fadd.s fa1,fa1,fa4 #sum+=4/(i*2-1)
         addi a3,a3,1 #i+=1
-        bne a3,a6,loop
+        blt a3,a6,loop
     fmv.s fa0,fa1
-    li a7,7
+    li a7,2
     ecall
 
     li a7,10    
